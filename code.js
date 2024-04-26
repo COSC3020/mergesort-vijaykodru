632ce495bb75d2a01fb9398c93ac0123f2256cb1
Vijay Kodru <123325232+vijaykodru@users.noreply.github.com>
2024-04-26 17:55:20 +0000

    test 1

--- code.js
+++ code.js
@@ -6,26 +6,32 @@ function mergesort(array) {
     else{
         for(let i = 1; i < length; i *= 2){
             for(let LE = 0; LE < length - 1; LE += 2*i){
-                let Mid = LE + i - 1
-                let RE = Math.min(LE + (2 * (i - 1)), length - 1)
+                let Mid = Math.min(LE + i - 1, length - 1);
+                let RE = Math.min(LE + (2 * i - 1), length - 1);
+
     
                 let LS = LE;
                 let RS = Mid + 1;
+                let merged = [];
     
-                while(LS <= Mid && RS <= RE){
-                    if(array[LS] <= array[RS]){
-                        LS++
+                while (LS <= Mid && RS <= RE) {
+                    if (array[LS] <= array[RS]) {
+                        merged.push(array[LS++]);
+                    } else {
+                        merged.push(array[RS++]);
                     }
-                    else{
-                        let temp = array[RS];
-                        for(let j = RS; j > LS; j--){
-                            array[j] = array[j - 1];
-                        }
-                        array[LS] = temp;
-                        LS++;
-                        Mid++;
-                        RS++;
-                    }
+                }
+    
+                while (LS <= Mid) {
+                    merged.push(array[LS++]);
+                }
+    
+                while (RS <= RE) {
+                    merged.push(array[RS++]);
+                }
+    
+                for (let k = LE; k <= RE; k++) {
+                    array[k] = merged[k - LE];
                 }
             }
         }
